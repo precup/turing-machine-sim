89cd2b688fa76184c8def5f8a19217140c3d7c64
precup <vincent@nerfrevolution.com>
2014-11-28 20:26:34 -0700

    Updated machine.js
    Now allows for saving and loading of state.

--- machine.js
+++ machine.js
@@ -1,60 +1,90 @@
-var buildMachine = function (data) {
+var buildMachine = function (dataArg) {
   var nodeNum = 0;
-  data.nodes.forEach(function (d) { 
+  dataArg.nodes.forEach(function (d) { 
     d.index = nodeNum++; 
-    d.name = "" + d.index;
+    d.name = "n" + d.index;
   });
-  data.links.forEach(function (d) { 
-    d.source = data.nodes[d.source]; 
-    d.target = data.nodes[d.target]; 
+  dataArg.links.forEach(function (d) { 
+    d.source = dataArg.nodes[d.source]; 
+    d.target = dataArg.nodes[d.target]; 
   });
   
-  function findNode(index) {
-    for(var i = 0; i < data.nodes.length; i++) {
-      if(data.nodes[i].index == index) {
-        return i;
+  var machine = {
+    data: dataArg,
+    findNode: function (index) {
+      for(var i = 0; i < machine.data.nodes.length; i++) {
+        if(machine.data.nodes[i].index == index) {
+          return i;
+        }
       }
-    }
-    return -1;
-  }
-  
-  function areLinked(index1, index2) {
-    for(var i = 0; i < data.links.length; i++) {
-      if(data.links[i].source.index == index1 || data.links[i].target.index == index2) {
-        return true;
+      return -1;
+    },
+    areLinked: function (index1, index2) {
+      for(var i = 0; i < machine.data.links.length; i++) {
+        if(machine.data.links[i].source.index == index1 && machine.data.links[i].target.index == index2) {
+          return true;
+        }
       }
-    }
-    return false;
-  }
-  
-  return {
+      return false;
+    },
+    setData: function(newData) { 
+      machine.data = newData; 
+      machine.data.links.forEach(function (d) { 
+        d.source = machine.data.nodes[machine.findNode(d.source)]; 
+        d.target = machine.data.nodes[machine.findNode(d.target)]; 
+      });
+      nodeNum = 0;
+      machine.data.nodes.forEach(function (d) { 
+        nodeNum = Math.max(nodeNum, d.index + 1);
+      });
+    },
+    getSaveData: function() {
+      var saveData = JSON.parse(JSON.stringify(machine.data));
+      saveData.links.forEach(function(link){
+        link.source = link.source.index;
+        link.target = link.target.index;
+      });
+      return saveData;
+    },
     getData: function() {
-      return data;
+      return machine.data;
     },
     addNode: function(node) {
       node.index = nodeNum++;
-      data.nodes.push(node);
+      node.name = "n" + node.index;
+      machine.data.nodes.push(node);
     },
-    addLink: function(startNode, endNode) {
-      var link = {
-        source: data.nodes[findNode(startNode)],
-        target: data.nodes[findNode(endNode)]
-      };
-      data.links.push(link);
+    toggleLink: function(startNode, endNode) {
+      if(machine.areLinked(startNode, endNode)) {
+        for(var i = 0; i < machine.data.links.length; i++) {
+          if(machine.data.links[i].source.index == startNode && machine.data.links[i].target.index == endNode) {
+            machine.data.links.splice(i, 1);
+            i--;
+          }
+        }
+      } else {
+        var link = {
+          source: machine.data.nodes[machine.findNode(startNode)],
+          target: machine.data.nodes[machine.findNode(endNode)]
+        };
+        machine.data.links.push(link);
+      }
     },
     deleteNode: function(num) {
-      for(var i = 0; i < data.links.length; i++) {
-        if(data.links[i].source.index == num || data.links[i].target.index == num) {
-          data.links.splice(i, 1);
+      for(var i = 0; i < machine.data.links.length; i++) {
+        if(machine.data.links[i].source.index == num || machine.data.links[i].target.index == num) {
+          machine.data.links.splice(i, 1);
           i--;
         }
       }
-      for(var i = 0; i < data.nodes.length; i++) {
-        if(data.nodes[i].index == num) {
-          data.nodes.splice(i, 1);
+      for(var i = 0; i < machine.data.nodes.length; i++) {
+        if(machine.data.nodes[i].index == num) {
+          machine.data.nodes.splice(i, 1);
           i--;
         }
       }
     }
   }
+  
+  return machine;
 }
