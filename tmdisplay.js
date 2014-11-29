00dfa122f63ea89fcac743b9fead2e41bace78b7
precup <vincent@nerfrevolution.com>
2014-11-28 20:29:36 -0700

    Updated tmdisplay.js
    Massive update. Away from a computer with git, hence the lack of commits. This adds labels to the nodes and an inner circle so that reject and accept states can be created just by classing the group correctly. Added the start of the code needed for start states. The code finally handles update from enter update exit properly.

--- tmdisplay.js
+++ tmdisplay.js
@@ -1,6 +1,5 @@
 var setupGUI = function (width, height, element, machine) {
-  var nodeRadius = 30,
-    shiftKey, ctrlKey, linkStart;
+  var nodeRadius = 30, innerRadius = 24, shiftKey, ctrlKey, linkStart, mouseData;
     
   d3.select("body")
       .on("keydown.brush", keydown)
@@ -8,21 +7,69 @@ var setupGUI = function (width, height, element, machine) {
       .each(function() { this.focus(); });
       
   var svg = d3.select(element).append("svg")
+      .attr("id", "vis")
       .attr("width", width)
       .attr("height", height);
+      
+  svg.append('svg:defs').append('svg:marker')
+      .attr('id', 'end-arrow')
+      .attr('viewBox', '0 -50 100 100')
+      .attr('refX', 36)
+      .attr('markerWidth', 30)
+      .attr('markerHeight', 30)
+      .attr('orient', 'auto')
+    .append('svg:path')
+      .attr('d', 'M 0,-5 L 10,0 L 0,5 z')
+      .attr('fill', 'black');
+      
+  svg.append('svg:defs').append('svg:marker')
+      .attr('id', 'light-arrow')
+      .attr('viewBox', '0 -50 100 100')
+      .attr('refX', 36)
+      .attr('markerWidth', 30)
+      .attr('markerHeight', 30)
+      .attr('orient', 'auto')
+    .append('svg:path')
+      .classed("double", true)
+      .attr('d', 'M 0,-5 L 10,0 L 0,5 z')
+      .attr('fill', '#999');
+      
+  svg.append('svg:defs').append('svg:marker')
+      .attr('id', 'mouse-arrow')
+      .attr('viewBox', '0 -50 100 100')
+      .attr('refX', 13)
+      .attr('markerWidth', 30)
+      .attr('markerHeight', 30)
+      .attr('orient', 'auto')
+    .append('svg:path')
+      .attr('d', 'M 0,-5 L 10,0 L 0,5 z')
+      .attr('fill', 'black');
 
   var link = svg.append("g")
       .attr("class", "link");
       
-  var mouseLine = svg.append("line").classed("link hidden", true);
+  var mouseLine = svg.append("path").classed("hidden", true)
+        .style('marker-end', function() { 
+            return machine.areLinked(linkStart, linkStart) ? 'url(#light-arrow)' : 'url(#end-arrow)';
+          })
+        .attr("fill", "none")
+        .classed('double', machine.areLinked(linkStart, linkStart));
+  
+  var startLine = svg.append("path").classed("hidden", true)
+        .style('marker-end', 'url(#end-arrow)')
+        .attr("fill", "none");
 
   var brush = buildBrush();
   brush.on("mouseup", function () {
       mouseLine.classed("hidden", true);
     })
     .on("mousemove", function (d) {
-      if(ctrlKey) {
-        mouseLine.attr("x2", d3.mouse(svg[0][0])[0]).attr("y2", d3.mouse(svg[0][0])[1]);
+      if(!mouseLine.classed("hidden")) {
+        mouseData[1].x = d3.mouse(svg[0][0])[0];
+        mouseData[1].y = d3.mouse(svg[0][0])[1];
+        mouseLine.attr("d", lineFunction(mouseData))
+          .style('marker-end', 'url(#mouse-arrow)')
+          .classed('double', false);
       }
     });
 
@@ -31,86 +78,156 @@ var setupGUI = function (width, height, element, machine) {
 
   var graph = machine.getData();
   
+  var straightLineFunction = d3.svg.line()
+      .x(function(d) { return d.x; })
+      .y(function(d) { return d.y; })
+      .interpolate("linear");
+  var curvedLineFunction = d3.svg.line()
+      .x(function(d) { return d.x; })
+      .y(function(d) { return d.y; })
+      .interpolate("basis");
+  var lineFunction = function(data) {
+    if(data[0].x == data[1].x && data[0].y == data[1].y) {
+      return curvedLineFunction([
+        data[0],
+        { x: data[0].x - 50, y: data[0].y + 30 },
+        { x: data[0].x - 70, y: data[0].y },
+        { x: data[0].x - 50, y: data[0].y - 30 },
+        data[1]
+      ]);
+    } else {
+      return straightLineFunction(data);
+    }
+  }
+      
   _update();
 
   function _update() {
     graph = machine.getData();
-    var links = link.selectAll("line").data(graph.links);
     
-    links.enter().append("line")
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+    /*startLine.attr("d", function(d) { 
+        return lineFunction(mouseData)})
+      .classed("hidden", typeof myVar === 'undefined');*/
+    
+    var links = link.selectAll("path")
+      .data(graph.links, function(d) { return d.source.index + " " + d.target.index; });
+    
+    links.enter().append("path")
+        .style('marker-end', 'url(#end-arrow)')
+        .attr("fill", "none");
         
-    links
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; })
+    links.attr("d", function(d) { 
+        return lineFunction([
+          { x: d.source.x, y: d.source.y },
+          { x: d.target.x, y: d.target.y }
+        ])})
         
     links.exit().remove();
 
-    var nodes = node.selectAll("g").data(graph.nodes);
+    var nodes = node.selectAll("g").data(graph.nodes, function(d) { return d.index; });
     var nodeGroup = nodes.enter().append("g");
-      
-    nodeGroup.append("circle")
+    
+    applyNodeListeners(
+      nodeGroup.append("circle")
+        .classed("outer", true)
         .attr("r", nodeRadius)
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; })
-        .on("mousedown", function(d) {
-          if(ctrlKey) {
-            linkStart = d.index;
-            mouseLine.classed("hidden", false)
-              .attr("x1", d.x)
-              .attr("y1", d.y)
-              .attr("x2", d.x)
-              .attr("y2", d.y);
-          } else {
-            if (!d.selected) {
-              if (!shiftKey) node.selectAll("g").select("circle").classed("selected", function(p) { return p.selected = d === p; });
-              else d3.select(this).classed("selected", d.selected = true);
-            }
-          }
-        })
-        .on("mousemove", function(d) {
-          if(ctrlKey) {
-            mouseLine.attr("x2", d.x).attr("y2", d.y);
-          }
-        })
-        .on("mouseup", function(d) {
-          if(ctrlKey) {
-            mouseLine.classed("hidden", true);
-            machine.addLink(linkStart, d.index);
-            _update();
-          }
-        })
-        .call(d3.behavior.drag()
-          .on("drag", function(d) { 
-            if(!ctrlKey) {
-              moveSelected(d3.event.dx, d3.event.dy); 
-            }
-          }));
+    );
+    
+    applyNodeListeners(
+      nodeGroup.append("circle")
+        .classed("inner", true)
+        .attr("r", innerRadius)
+    );
+      
+    nodeGroup.append("text");
           
-    nodes.select("circle").attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; })
-        .classed("selected", function(d) { return d.selected; });
+    nodes.classed("selected", function(d) { return d.selected; })
+      .select("circle.inner")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+      
+    nodes.select("circle.outer")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      
+    nodes.select("text")
+      .html(function (d) { return d.name; })
+      .attr("x", function(d) { return d.x; })
+      .attr("y", function(d) { return d.y; })
           
     nodes.exit().remove();
   }
+  
+  function applyNodeListeners(d) {
+    d.on("mousedown", function(d) {
+        if(ctrlKey) {
+          linkStart = d.index;
+          mouseData = [
+              { x: d.x, y: d.y },
+              { x: d.x, y: d.y }
+            ];
+          mouseLine.classed("hidden", false)
+            .attr("d", function(d) { 
+            return lineFunction(mouseData)})
+            .style('marker-end', function() { 
+              return machine.areLinked(linkStart, d.index) ? 'url(#light-arrow)' : 'url(#end-arrow)';
+            })
+            .classed('double', machine.areLinked(linkStart, d.index));
+        } else {
+          if (!d.selected) {
+            if (!shiftKey) node.selectAll("g").classed("selected", function(p) { return p.selected = d === p; });
+            else d3.select(this.parentNode).classed("selected", d.selected = true);
+          }
+        }
+      })
+      .on("mousemove", function(d) {
+        if(!mouseLine.classed("hidden")) {
+        mouseData[1].x = d.x;
+        mouseData[1].y = d.y;
+        mouseLine.attr("d", lineFunction(mouseData))
+        .style('marker-end', function() { 
+          return machine.areLinked(linkStart, d.index) ? 'url(#light-arrow)' : 'url(#end-arrow)';
+          })
+          .classed('double', machine.areLinked(linkStart, d.index));
+        }
+      })
+      .on("mouseup", function(d) {
+        if(ctrlKey && !mouseLine.classed("hidden")) {
+        mouseLine.classed("hidden", true);
+        machine.toggleLink(linkStart, d.index);
+        _update();
+        }
+      })
+      .call(d3.behavior.drag()
+        .on("drag", function(d) { 
+        if(!ctrlKey) {
+          moveSelected(d3.event.dx, d3.event.dy); 
+        }
+      }));
+  }
 
   function moveSelected(dx, dy) {
-    node.selectAll("g").select("circle").filter(function(d) { return d.selected; })
-        .attr("cx", function(d) { return d.x += dx; })
-        .attr("cy", function(d) { return d.y += dy; })
+    node.selectAll("g").select("text").filter(function(d) { return d.selected; })
+        .attr("x", function(d) { return d.x += dx; })
+        .attr("y", function(d) { return d.y += dy; })
+        
+    node.selectAll("g").selectAll("circle").filter(function(d) { return d.selected; })
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
 
-    link.selectAll("line").filter(function(d) { return d.source.selected; })
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; });
+    link.selectAll("path").filter(function(d) { return d.source.selected; })
+        .attr("d", function(d) { 
+          return lineFunction([
+            { x: d.source.x, y: d.source.y },
+            { x: d.target.x, y: d.target.y }
+          ])});
 
-    link.selectAll("line").filter(function(d) { return d.target.selected; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+    link.selectAll("path").filter(function(d) { return d.target.selected; })
+        .attr("d", function(d) { 
+          return lineFunction([
+            { x: d.source.x, y: d.source.y },
+            { x: d.target.x, y: d.target.y }
+          ])});
   }
 
   function keydown() {
@@ -137,7 +254,7 @@ var setupGUI = function (width, height, element, machine) {
           })
           .on("brush", function() {
             var extent = d3.event.target.extent();
-            node.selectAll("g").select("circle").classed("selected", function(d) {
+            node.selectAll("g").classed("selected", function(d) {
               return d.selected = d.previouslySelected ^
                   (extent[0][0] <= d.x && d.x < extent[1][0]
                   && extent[0][1] <= d.y && d.y < extent[1][1]);
