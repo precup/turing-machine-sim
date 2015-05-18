37e512dbbd7902a90859f6081dd2ad814b583c29
Mike Precup <vincent@nerfrevolution.com>
2015-05-18 15:59:25 -0700

    Improved pset name formatting

--- psets.js
+++ psets.js
@@ -3,28 +3,28 @@ var psets =
     {
       name: "Problem Set 3",
       problems: [
-        { name: "1: Bacon ipsum", charSet: "dy", type: "dfa" },
-        { name: "2: Dolor amet", charSet: "dy", type: "dfa" },
-        { name: "3: Flank meatloaf", charSet: "dy", type: "nfa" }
+        { name: "Problem 1: Bacon ipsum", charSet: "dy", type: "dfa" },
+        { name: "Problem 2: Dolor amet", charSet: "dy", type: "dfa" },
+        { name: "Problem 3: Flank meatloaf", charSet: "dy", type: "nfa" }
       ]
     },
     {
       name: "Problem Set 4",
         problems: [
-        { name: "1: Short loin", charSet: "abc", type: "dfa" },
-        { name: "2: T-bone alcatra", charSet: "abc", type: "dfa" },
-        { name: "3: Filet mignon", charSet: "abc", type: "dfa" },
-        { name: "4: Beef leberkas", charSet: "01", type: "nfa" },
-        { name: "5: Picanha ham", charSet: "01", type: "nfa" },
-        { name: "6: Pig shoulder", charSet: "01", type: "nfa" }
+        { name: "Problem 1: Short loin", charSet: "abc", type: "dfa" },
+        { name: "Problem 2: T-bone alcatra", charSet: "abc", type: "dfa" },
+        { name: "Problem 3: Filet mignon", charSet: "abc", type: "dfa" },
+        { name: "Problem 4: Beef leberkas", charSet: "01", type: "nfa" },
+        { name: "Problem 5: Picanha ham", charSet: "01", type: "nfa" },
+        { name: "Problem 6: Pig shoulder", charSet: "01", type: "nfa" }
       ]
     },
     {
       name: "Problem Set 5",
         problems: [
-        { name: "3: Chuck shank", charSet: "0123", type: "turing" },
-        { name: "4: Chicken boudin", charSet: "0123", type: "turing" },
-        { name: "5: Corned beef", charSet: "0123", type: "turing" }
+        { name: "Problem 3: Chuck shank", charSet: "0123", type: "turing" },
+        { name: "Problem 4: Chicken boudin", charSet: "0123", type: "turing" },
+        { name: "Problem 5: Corned beef", charSet: "0123", type: "turing" }
       ]
     }
   ];
