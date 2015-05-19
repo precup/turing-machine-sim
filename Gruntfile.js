307026f13dbb6252f96f7706a0122079fbfc9910
Jay Moon <jaymoon@stanford.edu>
2015-05-19 00:13:56 -0700

    added grunt, added grunt minification tasks, now using minified js files

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    uglify: {
+      my_target : {
+        options : {
+          sourceMap : true,
+          sourceMapName : 'sourceMap.map'
+        },
+        files : {
+          'tm.min.js' : [
+            '*.js',
+            'edges/*.js',
+            'nodes/*.js',
+            'menu/*.js',
+            'menu/modal/*.js',
+            'simulation/*.js',
+            'server/*.js'
+          ],
+          'select.min.js' : [
+            'psets.js', 
+            'util.js', 
+            'server/server.js', 
+            'server/mock.js'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+};
+
+
