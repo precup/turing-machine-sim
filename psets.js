e424b0e8bdf132e7a9587e5b6a780e3ac3d14456
Max Wang <maxwang7@stanford.edu>
2015-05-19 23:55:20 -0700

    use real psets

--- psets.js
+++ psets.js
@@ -1,33 +1,43 @@
 var psets =
   [
+    // {
+    //   id: 3,
+    //   name: "Problem Set 3",
+    //   problems: [
+    //     { id: 1, name: "Problem 1: Bacon ipsum", charSet: "dy", type: "dfa" },
+    //     { id: 2, name: "Problem 2: Dolor amet", charSet: "dy", type: "dfa" },
+    //     { id: 3, name: "Problem 3: Flank meatloaf", charSet: "dy", type: "nfa" }
+    //   ]
+    // },
+    // {
+    //   id: 4,
+    //   name: "Problem Set 4",
+    //     problems: [
+    //     { id: 1, name: "Problem 1: Short loin", charSet: "abc", type: "dfa" },
+    //     { id: 2, name: "Problem 2: T-bone alcatra", charSet: "abc", type: "dfa" },
+    //     { id: 3, name: "Problem 3: Filet mignon", charSet: "abc", type: "dfa" },
+    //     { id: 4, name: "Problem 4: Beef leberkas", charSet: "01", type: "nfa" },
+    //     { id: 5, name: "Problem 5: Picanha ham", charSet: "01", type: "nfa" },
+    //     { id: 6, name: "Problem 6: Pig shoulder", charSet: "01", type: "nfa" }
+    //   ]
+    // },
+    // {
+    //   id: 5,
+    //   name: "Problem Set 5",
+    //     problems: [
+    //     { id: 3, name: "Problem 3: Chuck shank", charSet: "0123", type: "tm" },
+    //     { id: 4, name: "Problem 4: Chicken boudin", charSet: "0123", type: "tm" },
+    //     { id: 5, name: "Problem 5: Corned beef", charSet: "0123", type: "tm" }
+    //   ]
+    // },
     {
-      id: 3,
-      name: "Problem Set 3",
+      id: 7,
+      name: "Problem Set 6",
       problems: [
-        { id: 1, name: "Problem 1: Bacon ipsum", charSet: "dy", type: "dfa" },
-        { id: 2, name: "Problem 2: Dolor amet", charSet: "dy", type: "dfa" },
-        { id: 3, name: "Problem 3: Flank meatloaf", charSet: "dy", type: "nfa" }
-      ]
-    },
-    {
-      id: 4,
-      name: "Problem Set 4",
-        problems: [
-        { id: 1, name: "Problem 1: Short loin", charSet: "abc", type: "dfa" },
-        { id: 2, name: "Problem 2: T-bone alcatra", charSet: "abc", type: "dfa" },
-        { id: 3, name: "Problem 3: Filet mignon", charSet: "abc", type: "dfa" },
-        { id: 4, name: "Problem 4: Beef leberkas", charSet: "01", type: "nfa" },
-        { id: 5, name: "Problem 5: Picanha ham", charSet: "01", type: "nfa" },
-        { id: 6, name: "Problem 6: Pig shoulder", charSet: "01", type: "nfa" }
-      ]
-    },
-    {
-      id: 5,
-      name: "Problem Set 5",
-        problems: [
-        { id: 3, name: "Problem 3: Chuck shank", charSet: "0123", type: "tm" },
-        { id: 4, name: "Problem 4: Chicken boudin", charSet: "0123", type: "tm" },
-        { id: 5, name: "Problem 5: Corned beef", charSet: "0123", type: "tm" }
+        { id: 1, name: "Problem One: Binary Sorting", charSet: "01", type: "tm"},
+        { id: 2, name: "Problem Two i: The Collatz Conjecture", charSet: "1", type: "tm" },
+        { id: 3, name: "Problem Two ii: The Collatz Conjection", charSet: "1", type: "tm" },
+        { id: 4, name: "Problem Two iii: The Collatz Conjection", charSet: "1", type: "tm" }
       ]
     }
   ]
